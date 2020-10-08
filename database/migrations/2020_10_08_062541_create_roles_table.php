1f066ba57084ca3e6db66e06fca7221f85ded643
Trojan4ek <serjderevjanko@gmail.com>
2020-10-08 10:38:24 +0300

    fix column in migration

--- database/migrations/2020_10_08_062541_create_roles_table.php
+++ database/migrations/2020_10_08_062541_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->enum('title', ['user', 'admin']);
+            $table->string('title');
             $table->timestamps();
         });
     }
