a9e9e0f498500771c427d5d521c581fc08dc05f0
Trojan4ek <serjderevjanko@gmail.com>
2020-10-20 11:04:57 +0300

    add translation

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -6,8 +6,8 @@
     <div>
         <div class="bg-red text-center pt-4 pb-4">
             <img class="rounded mx-auto d-block" src="{{ asset('images/StopFraud_960x432.png') }}" alt="">
-            <h2 class="text-white">Всеукраинская база данных информации о мошенниках</h2>
-            <a class="btn btn-danger btn-lg align-center" href="{{route('fraud.create')}}">Сообщить о мошеннике</a> <br>
+            <h2 class="text-white">{{__('messages.main.header')}}</h2>
+            <a class="btn btn-danger btn-lg align-center" href="{{route('fraud.create')}}">{{__('messages.main.button')}}</a> <br>
         </div>
     </div>
 
@@ -15,44 +15,35 @@
         <div class="row pt-4 pb-4">
             <div class="col-lg-4 text-center">
                 <a href="{{route('advices')}}"><img src="{{ asset('images/StopFraudFinger0_600-300x300.png') }}" alt=""></a>
-                <h3>Ответь жулику правильно</h3>
+                <h3>{{__('messages.main.button1_header')}}</h3>
                 <p>
-                    Что нельзя делать при звонке с неизвестного номера? Как вести себя при телефонном разговоре с
-                    потенциальным жуликом?
+                    {{__('messages.main.button1_text')}}
                 </p>
                 <p>
-                    <strong>
-                        Ознакомьтесь с основными правилами и советами поведения при телефонном разговоре с неизвестным
-                        абонентом!
-                    </strong>
+                    <strong>{{__('messages.main.button1_description')}}</strong>
                 </p>
-                <a class="btn btn-danger btn-lg" href="{{route('advices')}}">Читать советы</a></div>
+                <a class="btn btn-danger btn-lg" href="{{route('advices')}}">{{__('messages.main.button1')}}</a></div>
             <div class="col-lg-4 text-center">
                 <a href="{{route('fraud.index')}}"><img src="{{ asset('images/StopFraudFinger1_600-300x300.png') }}" alt=""></a>
-                <h3>Пробей мошенника по базе</h3>
+                <h3>{{__('messages.main.button2_header')}}</h3>
                 <p>
-                    Собираетесь совершить покупку через Интернет? Потеряли что-либо и теперь вам звонят с предложениями
-                    вернуть утрату за деньги?
+                    {{__('messages.main.button2_text')}}
+
                 </p>
                 <p>
-                    <strong> Проверьте базу данных FraudHunt - не станьте жертвой мошенников!</strong>
+                    <strong>{{__('messages.main.button2_description')}}</strong>
                 </p>
-                <a class="btn btn-danger btn-lg" href="{{route('fraud.index')}}">Проверить в базе</a></div>
+                <a class="btn btn-danger btn-lg" href="{{route('fraud.index')}}">{{__('messages.main.button2')}}</a></div>
             <div class="col-lg-4 text-center">
                 <a href="{{route('fraud.create')}}"><img src="{{ asset('images/StopFraudFinger3_600-300x300.png') }}" alt=""></a>
-                <h3>Сообщи об аферисте</h3>
+                <h3>{{__('messages.main.button3_header')}}</h3>
                 <p>
-                    Чуть не попались на удочку мошенникам? Столкнулись с кибер-аферистами, которые пытались развести вас
-                    на
-                    деньги?
+                    {{__('messages.main.button3_text')}}
                 </p>
                 <p>
-                    <strong>
-                        Сделайте доброе дело, сообщите об этом друзьям и знакомым! Помогите им не быть обманутыми
-                        аферистами!
-                    </strong>
+                    <strong>{{__('messages.main.button3_description')}}</strong>
                 </p>
-                <a class="btn btn-danger btn-lg" href="{{route('fraud.create')}}">Сообщить о жулике</a></div>
+                <a class="btn btn-danger btn-lg" href="{{route('fraud.create')}}">{{__('messages.main.button3')}}</a></div>
         </div>
     </div>
 
@@ -60,7 +51,7 @@
         <div class="container">
             <div class="row">
                 <h1 class="text-left">
-                    Мобильное приложение - FraudHunt
+                    {{__('messages.main.app_header')}}
                 </h1>
             </div>
             <div class="row">
@@ -68,13 +59,10 @@
                     <img src="{{asset('images/AppFraudHunt_screen_news-300x300.png')}}" alt=""></div>
                 <div class="col-lg-8 align-left">
                     <h3>
-                        FraudHunt - это мобильное приложение, которое даёт возможность добавлять в адресную книгу Вашего
-                        телефона номера мошенников, жуликов и прочих телефонных аферистов, собранных в всеукраинской
-                        базе
-                        данных "FraudHunt".
+                        {{__('messages.main.app_text')}}
                     </h3>
                     <p>
-                        Не дай мошенникам шанса, загрузи FraudHunt !
+                        {{__('messages.main.app_advice')}}
                     </p>
                     <p>
                         <a href="https://play.google.com/store/apps/details?id=com.klevrit.fraudhunt" target="_blank">
